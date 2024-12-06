85ee63c1a8131d90f4eb6854c0c7f95e8a0162c9
ghter8 <58772830+ghter8@users.noreply.github.com>
2024-12-06 15:48:33 +0800

    MyEmoji
    Emoji list

---
+++ myEmoji.py
@@ -0,0 +1,18 @@
+import discord #type: ignore
+from discord import PartialEmoji #type: ignore
+
+emoji_list = {
+    "coal": PartialEmoji.from_str("<:coal:1314484176765980722>"),
+    "gold": PartialEmoji.from_str("<:gold:1314484199054512229>"),
+    "crystal": PartialEmoji.from_str("<:crystal:1314484211327172628>"),
+    "branch": PartialEmoji.from_str("<:branch:1314484235805003867>"),
+    "bark": PartialEmoji.from_str("<:bark:1314484251139244092>"),
+    "resin": PartialEmoji.from_str("<:resin:1314484265395945572>"),
+    "beef": PartialEmoji.from_str("<:beef:1314484277697577030>"),
+    "lamb": PartialEmoji.from_str("<:lamb:1314484288720474162>"),
+    "lizard": PartialEmoji.from_str("<:lizard:1314484301668028416>"),
+    "salmon": PartialEmoji.from_str("<:salmon:1314484315433730090>"),
+    "cod": PartialEmoji.from_str("<:cod:1314484332488032257>"),
+    "pufferfish": PartialEmoji.from_str("<:pufferfish:1314484343699406868>"),
+    "bag": PartialEmoji.from_str("<:bag:1314484354768044105>"),
+}
