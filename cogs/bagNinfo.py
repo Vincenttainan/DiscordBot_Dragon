27bafb384d8b575ae7c6ab0cfe20c9142d94760a
Vincenttainan <54768760+Vincenttainan@users.noreply.github.com>
2024-12-06 10:56:41 +0800

    Create bagNinfo.py

---
+++ cogs/bagNinfo.py
@@ -0,0 +1,74 @@
+import discord
+
+import sys
+sys.path.append('/Users/vincenttainan/Desktop/pythonCSTP')
+
+import myJson
+import myRand
+
+from enum import Enum
+from discord.ext import commands, tasks
+from discord import app_commands
+
+#========================================================================================================================================================================================================#
+
+red = 0xff0000
+blue = 0x0000ff
+green = 0x00ff00
+
+
+photos = {
+    # all of the photos are needed to be redrawed with the size of 320 x 320 (pixels) or maybe 16 x 16 blocks (20 pixels for a block)
+    "bag" : "https://i.imgur.com/RSKeSAD.png"
+}
+
+
+#========================================================================================================================================================================================================#
+
+class Bag(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bag Module Ready.")
+
+    @app_commands.command(name = "bag", description = "Seeking items in your bag.")
+    async def bag(self, ctx: commands.context):
+        user_id = str(ctx.user.id)
+        
+        embed = discord.Embed(
+            title = "<@"+user_id+"> 's bag", 
+            color = green
+        )
+
+        embed.set_thumbnail(url = photos["bag"])
+
+        embed.add_field(name = "Mine", value = "", inline=False)
+        embed.add_field(name = "", value = "Coal : `"+str(myJson.get_value(user_id,"resource","coal"))+"`", inline=True)
+        embed.add_field(name = "", value = "Gold : `"+str(myJson.get_value(user_id,"resource","gold"))+"`", inline=True)
+        embed.add_field(name = "", value = "Crystal : `"+str(myJson.get_value(user_id,"resource","crystal"))+"`", inline=True)
+
+        embed.add_field(name = "Wood", value = "", inline=False)
+        embed.add_field(name = "", value = "Branch : `"+str(myJson.get_value(user_id,"resource","branch"))+"`", inline=True)
+        embed.add_field(name = "", value = "Bark : `"+str(myJson.get_value(user_id,"resource","bark"))+"`", inline=True)
+        embed.add_field(name = "", value = "Resin : `"+str(myJson.get_value(user_id,"resource","resin"))+"`", inline=True)
+
+        embed.add_field(name = "Farm", value = "", inline=False)
+        embed.add_field(name = "", value = "Beef : `"+str(myJson.get_value(user_id,"resource","beef"))+"`", inline=True)
+        embed.add_field(name = "", value = "Lamb : `"+str(myJson.get_value(user_id,"resource","lamb"))+"`", inline=True)
+        embed.add_field(name = "", value = "Lizard : `"+str(myJson.get_value(user_id,"resource","lizard"))+"`", inline=True)
+
+        embed.add_field(name = "Fish", value = "", inline=False)
+        embed.add_field(name = "", value = "Salmon : `"+str(myJson.get_value(user_id,"resource","salmon"))+"`", inline=True)
+        embed.add_field(name = "", value = "Cod : `"+str(myJson.get_value(user_id,"resource","cod"))+"`", inline=True)
+        embed.add_field(name = "", value = "Pufferfish : `"+str(myJson.get_value(user_id,"resource","pufferfish"))+"`", inline=True)
+
+        await ctx.response.send_message(embed = embed)
+
+
+
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Bag(bot))
