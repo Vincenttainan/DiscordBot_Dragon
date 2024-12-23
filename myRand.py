98319a7ac0e1ebd513d8c74ea431103ef601e7d4
ghter8 <58772830+ghter8@users.noreply.github.com>
2024-12-23 13:45:01 +0800

    Update myRand.py
    New function:
        set_chance()

--- myRand.py
+++ myRand.py
@@ -5,3 +5,11 @@ def get_rand(a, b):
 	#隨機在a,b間(含a,b)取得一數，回傳一數
 	random.seed(time.time())
 	return random.randint(a, b)
+
+def set_chance(weight: list, options: list = []):
+	# 以設定好的權重進行抽取
+	if len(options) != len(weight):
+		options = range(1, len(weight)+1)
+
+	random.seed(time.time())
+	return random.choices(options, weight)[0]
